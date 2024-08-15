90b1e5bdab0460d5b41792588269580682e03467
Justin <Justin@grandcircus.co>
2024-08-15 11:15:18 -0400

    msg

--- BranchesDemo/Program.cs
+++ BranchesDemo/Program.cs
@@ -1,2 +1,6 @@
 ﻿//List of products
 List<string> products = new List<string>();
+
+//Checking out
+Console.WriteLine("Enter credit card info");
+string card = Console.ReadLine();
--- BranchesDemo/obj/Debug/net8.0/BranchesDemo.AssemblyInfo.cs
+++ BranchesDemo/obj/Debug/net8.0/BranchesDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BranchesDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c14b5be2a55f9e90c72efe27316e56b8fddc50")]
 [assembly: System.Reflection.AssemblyProductAttribute("BranchesDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BranchesDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
