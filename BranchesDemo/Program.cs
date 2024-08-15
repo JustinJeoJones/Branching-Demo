b0ab9d315aaa9db6d932ec510ad05f794ce47a18
Justin <Justin@grandcircus.co>
2024-08-15 11:17:12 -0400

    finished payment

--- BranchesDemo/Program.cs
+++ BranchesDemo/Program.cs
@@ -4,3 +4,4 @@ List<string> products = new List<string>();
 //Checking out
 Console.WriteLine("Enter credit card info");
 string card = Console.ReadLine();
+Console.WriteLine($"Item purchased with {card}");
